85eef94b831047842dcf6581c562dc5cae506be1
BanPengfei <tubajiqing@163.com>
2018-06-08 17:39:21 +0800

    update TestDemo,testTime  vession -2.0

--- cmfz/src/test/java/TestDEmo.java
+++ cmfz/src/test/java/TestDEmo.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 public class TestDEmo {
@@ -11,5 +13,16 @@ public class TestDEmo {
             }
             System.out.println();
         };new HashMap();
+    }
+
+    @Test
+    public void testTime() throws InterruptedException {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date d=new Date();
+        String date=sdf.format(d);
+        Thread.sleep(3000);
+        Date d2=new Date();
+        String date2=sdf.format(d2);
+        System.out.println((d2.getTime()-d.getTime())/1000);
     }
 }
